fcb51d61e8bb2116636fd6d024d6c1fc8a49f63f
Colin Charles <charles.colin68@gmail.com>
2022-11-10 15:41:22 -0700

    first commit

---
+++ Game/Casting/Mineral.cs
@@ -0,0 +1,64 @@
+namespace Unit04.Game.Casting{
+        /// <summary>
+        /// <para>An item of cultural or historical interest.</para>
+        /// <para>
+        /// The responsibility of an Mineral is to provide a message about itself.
+        /// </para>
+        /// </summary>
+    class Mineral : Actor{
+        private int _value;
+        /// <summary>
+        /// Constructs a new instance of Mineral.
+        /// </summary>
+        public Mineral(){
+            _value = 0;
+        }
+
+        /// <summary>
+        /// Gets the mineral's message.
+        /// </summary>
+        /// <returns>The message as a string.</returns>
+        public int GetValue(){
+            return _value;
+        }
+    }
+}
+
+// namespace Unit04.Game.Casting
+// {
+//     // TODO: Implement the Artifact class here
+
+//     // 1) Add the class declaration. Use the following class comment. Make sure you
+//     //    inherit from the Actor class.
+
+//         /// <summary>
+//         /// <para>An item of cultural or historical interest.</para>
+//         /// <para>
+//         /// The responsibility of an Artifact is to provide a message about itself.
+//         /// </para>
+//         /// </summary>
+
+
+//     // 2) Create the class constructor. Use the following method comment.
+
+//         /// <summary>
+//         /// Constructs a new instance of Artifact.
+//         /// </summary>
+
+
+//     // 3) Create the GetMessage() method. Use the following method comment.
+
+//         /// <summary>
+//         /// Gets the artifact's message.
+//         /// </summary>
+//         /// <returns>The message as a string.</returns>
+
+
+//     // 4) Create the SetMessage(string message) method. Use the following method comment.
+
+//         /// <summary>
+//         /// Sets the artifact's message to the given value.
+//         /// </summary>
+//         /// <param name="message">The given message.</param>
+
+// }
